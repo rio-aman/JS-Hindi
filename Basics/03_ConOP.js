8a69015e3f8c5ed18ca1d56f78c649d17e8a9496
AmanX <130999468+rio-aman@users.noreply.github.com>
2024-04-20 08:17:12 +0000

    conversion and operation

--- Basics/03_ConOP.js
+++ Basics/03_ConOP.js
@@ -1,32 +1,57 @@
-// let score = "33abc" 
-// let score = null
-// let score = true
-let score = "aman"
+// // let score = "33abc" 
+// // let score = null
+// // let score = true
+// let score = "aman"
 
 // console.log(typeof score);
 
-// if we confiremd that we have to work on number then below things have to do 
+// // if we confiremd that we have to work on number then below things have to do 
 
-let valueNumber = Number(score)
+// let valueNumber = Number(score)
 
-// console.log(typeof valueNumber);
-// console.log(valueNumber); //NaN (not a number)
+// // console.log(typeof valueNumber);
+// // console.log(valueNumber); //NaN (not a number)
 
-//"33" - 33
-// "33abc" - NaN 
-// true - 1 / false - 0
+// //"33" - 33
+// // "33abc" - NaN 
+// // true - 1 / false - 0
 
-let isLoggedIn = null
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) ;
+// let isLoggedIn = null
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) ;
 
-// 1 - true / 0 - false
-// "" - false
-// "aman" - true 
-// "TRUE" - true
-// null - false
+// // 1 - true / 0 - false
+// // "" - false
+// // "aman" - true 
+// // "TRUE" - true
+// // null - false
 
-let someNumber = 468
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 468
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ====================================================OPERATION========================================================
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+let str1 = "hello"
+let str2 = " AMAN PAJI"
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1+2+"1")
+console.log(1+2+"1")
+console.log("1"+2+"1")
+console.log(+"")
+console.log(+true)
+
+// this not prefer as not suggested to write a complex code like above 
+
+let num1, num2, num3
+num1=num2=num3=2+2 //this also not prefer 
+
