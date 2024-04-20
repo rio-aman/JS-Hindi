559ccd8d003077be077de2a9fc06dd251cbf521c
AmanX <130999468+rio-aman@users.noreply.github.com>
2024-04-20 08:42:36 +0000

    datatypes summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// JavaScript is a dynamically typed language , which means that 
+// data types of variables are determined by the value they hold at runtime
+// and can change throughout the program as we assign different values to them.
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
