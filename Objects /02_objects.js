f3f42940c4f0fa4d17276c9f11581df9bbe3eea5
AmanX <130999468+rio-aman@users.noreply.github.com>
2024-06-26 13:23:43 +0000

    objects

---
+++ Objects /02_objects.js
@@ -0,0 +1,20 @@
+// const tinderUser = new Object() // singleton object 
+const tinderUser = {}  // non singleton object 
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Aman",
+            lastname: "Prajapati"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);
