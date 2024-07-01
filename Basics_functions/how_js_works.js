a6f670d70d17b5a905c6fcf8730ffb80e2aaf81c
AmanX <130999468+rio-aman@users.noreply.github.com>
2024-07-01 05:37:29 +0000

    how js works

---
+++ Basics_functions/how_js_works.js
@@ -0,0 +1,14 @@
+// Javascript Execution Context (ec)
+
+// sbsephle Global Execution Context to hogahehoga 
+
+// js is single threaded
+
+// there are three ec
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. eval Execution Context
+
+// phases at that js code runs
+// 1. memory creation phase or creation phase
+// 2. Execution phase
