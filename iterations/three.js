87255dd62a2840608d7ed2b07e670928945bf1c0
AmanX <130999468+rio-aman@users.noreply.github.com>
2024-07-17 15:16:14 +0000

    some new loops

---
+++ iterations/three.js
@@ -0,0 +1,47 @@
+// mostly these loops are arrays 
+
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+// for (const iterator of object) {  yaha object mtlb kis cheez pr iteration perform karna hai 
+// }
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
+
+objects can't 
